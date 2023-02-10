d4a1f8786da2fe5b59584ac9898db15eaa9b8972
helena <helena@helenas-MBP.hitronhub.home>
2023-02-10 14:57:05 -0800

    assignment

---
+++ js/app.js
@@ -0,0 +1,92 @@
+
+/***********/
+/* Strings */
+/***********/
+
+// Define and assign stringBefore1 here
+const stringBefore1 ="Hyeju"
+console.log('Hyeju');
+
+// Define and assign stringBefore2 here
+const stringBefore2 = "Hong"
+console.log('Hong');
+
+// Using stringBefore1 and stringBefore2,
+// Define and assign stringAfter, using template literals, here
+const stringAfter ='Hi, my name is ${stringBefore1} ${stringBefore2}'
+console.log('Hello, My name is Hyeju Hong');
+
+/***********/
+/* Numbers */
+/***********/
+
+// Define and assign numberBefore1 here
+const numberBefore1 ="10" // I create numberberBefore1 why is "10 because I like even number"
+
+// Define and assign numberBefore2 here
+const numberBefore2 = "40" // My favorite actor age is 40's and I used this number
+
+// Define and assign numberAfter1 here
+const numberAfter1 = "50" // Just want a make  this crazy.
+
+// Define and assign numberAfter2 here
+const numberAfter2 = "900"  // Crazy number so when we do math bit more harder
+
+// Define and assign numberAfter4 here 
+const numberAfter3 = "5.3" //Crazy number so when we do math bit more harder
+
+// Define and assign numberAfter5 here
+const numberAfter4 = "63" // I used my imagenation
+
+
+// Using numberBefore1 and numberBefore2,
+// Define and assign numberAfter1, numberAfter2, numberAfter3, numberAfter4 using different arithmetic operators
+
+console.log (`$${10} + $${40} * $${50} / $${900} + $${5.3} - $${63}`);
+console.log ('=45.4777778')
+
+
+/************************************/
+/* Do not edit code below this line */
+/************************************/
+
+/* This code will create errors until the above steps are completed */
+
+// Print String Before 1
+/* This code will create errors until the above steps are completed */
+
+// Print String Before 1
+const stringBefore1Output = document.querySelector('.string-before-1');
+stringBefore1Output.innerText = stringBefore1;
+
+// Print String Before 2
+const stringBefore2Output = document.querySelector('.string-before-2');
+stringBefore2Output.innerText = stringBefore2;
+
+// Print String After 2
+const stringAfterOutput = document.querySelector('.string-after');
+stringAfterOutput.innerText = stringAfter;
+
+// Print Number Before 1
+const numberBefore1Output = document.querySelector('.number-before-1');
+numberBefore1Output.innerText = numberBefore1;
+
+// Print Number Before 2
+const numberBefore2Output = document.querySelector('.number-before-2');
+numberBefore2Output.innerText = numberBefore2;
+
+// Print Number After 1
+const numberAfter1Output = document.querySelector('.number-after-1');
+numberAfter1Output.innerText = numberAfter1;
+
+// Print Number After 2
+const numberAfter2Output = document.querySelector('.number-after-2');
+numberAfter2Output.innerText = numberAfter2;
+
+// Print Number After 3
+const numberAfter3Output = document.querySelector('.number-after-3');
+numberAfter3Output.innerText = numberAfter3;
+
+// Print Number After 4
+const numberAfter4Output = document.querySelector('.number-after-4');
+numberAfter4Output.innerText = numberAfter4;
