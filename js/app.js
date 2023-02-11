a78a119af3bbe819135139c45d7c2b4e15776973
helena <helena@helenas-MBP.hitronhub.home>
2023-02-10 21:53:35 -0800

    inziliting

--- js/app.js
+++ js/app.js
@@ -5,45 +5,41 @@
 
 // Define and assign stringBefore1 here
 const stringBefore1 ="Hyeju"
-console.log('Hyeju');
+console.log(stringBefore1);
 
 // Define and assign stringBefore2 here
 const stringBefore2 = "Hong"
-console.log('Hong');
+console.log(stringBefore2);
 
 // Using stringBefore1 and stringBefore2,
 // Define and assign stringAfter, using template literals, here
-const stringAfter ='Hi, my name is ${stringBefore1} ${stringBefore2}'
-console.log('Hello, My name is Hyeju Hong');
+const stringAfter = ` My name is ${stringBefore1} ${stringBefore2}`
+console.log(stringAfter);
 
 /***********/
 /* Numbers */
 /***********/
 
 // Define and assign numberBefore1 here
-const numberBefore1 ="10" // I create numberberBefore1 why is "10 because I like even number"
+const numberBefore1 = 20 // I create numberberBefore1 why is "10 because I like even number"
 
 // Define and assign numberBefore2 here
-const numberBefore2 = "40" // My favorite actor age is 40's and I used this number
-
-// Define and assign numberAfter1 here
-const numberAfter1 = "50" // Just want a make  this crazy.
-
-// Define and assign numberAfter2 here
-const numberAfter2 = "900"  // Crazy number so when we do math bit more harder
-
-// Define and assign numberAfter4 here 
-const numberAfter3 = "5.3" //Crazy number so when we do math bit more harder
-
-// Define and assign numberAfter5 here
-const numberAfter4 = "63" // I used my imagenation
-
+const numberBefore2 = 40 // My favorite actor age is 40's and I used this number
 
 // Using numberBefore1 and numberBefore2,
 // Define and assign numberAfter1, numberAfter2, numberAfter3, numberAfter4 using different arithmetic operators
 
-console.log (`$${10} + $${40} * $${50} / $${900} + $${5.3} - $${63}`);
-console.log ('=45.4777778')
+const numberAfter1 = numberBefore1 + numberBefore2 
+console.log(numberAfter1)
+const numberAfter2 = numberBefore1 - numberBefore2
+console.log(numberAfter2)
+const numberAfter3 = numberBefore1 * numberBefore2
+console.log(numberAfter3)
+const numberAfter4 = numberBefore1 / numberBefore2
+console.log(numberAfter4)
+
+
+
 
 
 /************************************/
